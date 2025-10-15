2d8cc3a9b724209acc51aff8695404efa4607f46
SkellaTex <skellatex@proton.me>
2025-10-15 15:07:53 +0200

    Corrosion and Restoration revamp

--- src/main/java/net/skellatex/endelurgy/potion/EnervationEffect.java
+++ src/main/java/net/skellatex/endelurgy/potion/EnervationEffect.java
@@ -7,7 +7,6 @@ import net.minecraft.world.entity.ai.attributes.Attributes;
 
 public class EnervationEffect extends MobEffect {
 
-
     protected EnervationEffect() {
         super(MobEffectCategory.HARMFUL, 3276800);
         this.addAttributeModifier(Attributes.MAX_HEALTH, "91AEAA56-376B-4498-935B-2F7F68020638", -4F, AttributeModifier.Operation.ADDITION);
--- src/main/java/net/skellatex/endelurgy/potion/RestorationEffect.java
+++ src/main/java/net/skellatex/endelurgy/potion/RestorationEffect.java
@@ -1,9 +1,10 @@
 package net.skellatex.endelurgy.potion;
 
+import net.minecraft.world.InteractionHand;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import org.jetbrains.annotations.NotNull;
 
@@ -17,19 +18,64 @@ public class RestorationEffect extends MobEffect {
     }
 
     @Override
-    public void applyEffectTick(@NotNull LivingEntity livingEntity, int amplifier) {
-        if (livingEntity instanceof Player player && !livingEntity.level().isClientSide) {
-            for (int i = 0; i < player.getInventory().getContainerSize(); i++) {
-                ItemStack stack = player.getInventory().getItem(i);
-                if (!stack.isEmpty() && stack.isDamageableItem() && stack.getDamageValue() > 0) {
+    public void applyEffectTick(@NotNull LivingEntity pLivingEntity, int amplifier) {
+
+        ItemStack mainhand = pLivingEntity.getMainHandItem();
+        ItemStack offhand = pLivingEntity.getOffhandItem();
+        ItemStack head = pLivingEntity.getItemBySlot(EquipmentSlot.HEAD);
+        ItemStack chest = pLivingEntity.getItemBySlot(EquipmentSlot.CHEST);
+        ItemStack legs = pLivingEntity.getItemBySlot(EquipmentSlot.LEGS);
+        ItemStack feet = pLivingEntity.getItemBySlot(EquipmentSlot.FEET);
+
+        if (!pLivingEntity.level().isClientSide) {
+                if (!mainhand.isEmpty() && mainhand.isDamageableItem()) {
                     if (RANDOM.nextInt(16) == 0) {
-                        int repairAmount = 1 + amplifier;
-                        stack.setDamageValue(stack.getDamageValue() - repairAmount);
+                        int repair = 2 + amplifier * 2;
+                        mainhand.setDamageValue(mainhand.getDamageValue() - repair);
+                    }
+                }
+        }
+        if (!pLivingEntity.level().isClientSide) {
+                if (!offhand.isEmpty() && offhand.isDamageableItem()) {
+                    if (RANDOM.nextInt(16) == 0) {
+                        int repair = 2 + amplifier * 2;
+                        offhand.setDamageValue(offhand.getDamageValue() - repair);
+                    }
+                }
+        }
+        if (!pLivingEntity.level().isClientSide) {
+                if (!head.isEmpty() && head.isDamageableItem()) {
+                    if (RANDOM.nextInt(16) == 0) {
+                        int repair = 1 + amplifier;
+                        head.setDamageValue(head.getDamageValue() - repair);
+                    }
+                }
+        }
+        if (!pLivingEntity.level().isClientSide) {
+                if (!chest.isEmpty() && chest.isDamageableItem()) {
+                    if (RANDOM.nextInt(16) == 0) {
+                        int repair = 1 + amplifier;
+                        chest.setDamageValue(chest.getDamageValue() - repair);
+                    }
+                }
+        }
+        if (!pLivingEntity.level().isClientSide) {
+                if (!legs.isEmpty() && legs.isDamageableItem()) {
+                    if (RANDOM.nextInt(16) == 0) {
+                        int repair = 1 + amplifier;
+                        legs.setDamageValue(legs.getDamageValue() - repair);
+                    }
+                }
+        }
+        if (!pLivingEntity.level().isClientSide) {
+                if (!feet.isEmpty() && feet.isDamageableItem()) {
+                    if (RANDOM.nextInt(16) == 0) {
+                        int repair = 1 + amplifier;
+                        feet.setDamageValue(feet.getDamageValue() - repair);
                     }
                 }
             }
         }
-    }
 
     @Override
     public boolean isDurationEffectTick(int duration, int amplifier) {
